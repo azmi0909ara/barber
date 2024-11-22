1d52fd9a2e7d4dbc22de1a65af7a6b83bc2e4fc5
haritsrs <haritssetiono23@gmail.com>
2024-11-22 19:48:40 +0700

    firebase api key update

--- firebase.ts
+++ firebase.ts
@@ -2,7 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  // Your Firebase config here
+  apiKey: "AIzaSyA57D78rdd5x_RCNQT1EhBtuBdrdbFodLs",
+  authDomain: "barber-15b48.firebaseapp.com",
+  projectId: "barber-15b48",
+  storageBucket: "barber-15b48.firebasestorage.app",
+  messagingSenderId: "215556113867",
+  appId: "1:215556113867:web:30c08a848e185dcf0d224e",
+  measurementId: "G-0KZEYZWEFL"
 };
 
 export const app = initializeApp(firebaseConfig);
